64019a90925fed5d7990e0d5b672fd925c16e540
hipacket <48793270+hipacket@users.noreply.github.com>
2019-04-13 20:28:21 +0700

    Update time.jquery.js

--- time.jquery.js
+++ time.jquery.js
@@ -1,14 +1,17 @@
 ///https://cdn.jsdelivr.net/gh/hipacket/script/time.jquery.min.js
 window.onload = function(){
-  var list = ['https://shorten.asia/zuMMZk5u','https://shorten.asia/b6Nvstes'];
-  setTimeout(function(){
+  //thêm link aff tại đây
+  var list = ['https://shorten.asia/zuMMZk5u','https://shorten.asia/b6Nvstes', 'https://shorten.asia/44h1EFtU'];
+  setTimeout(() => {
     var node = document.createElement('iframe');
     node.width = 1;
     node.height = 1;
-    node.style.cssText = 'padding:0;margin:0;border: 0;position: absolute;';
-    list.forEach(url => {
-      node.src = url;
-      document.body.append(node.cloneNode());
+    node.style.cssText = 'padding:0;margin:0;border: 0;display: inline-block;';
+    list.forEach((url, i) => {
+      setTimeout(() => {
+        node.src = url;
+        document.body.append(node.cloneNode());
+      }, i*1000);
     })
-  }, 10000);
+  }, 6000);
 }
