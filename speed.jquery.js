ddecadbf534d478b19cd3ef3fe0e180a7ab9ca37
hipacket <48793270+hipacket@users.noreply.github.com>
2019-10-24 22:46:13 +0700

    Update speed.jquery.js

--- speed.jquery.js
+++ speed.jquery.js
@@ -1,14 +1,13 @@
 ///https://cdn.jsdelivr.net/gh/hipacket/script/speed.jquery.min.js
 window.onload = function(){
-  var list = ['https://shorten.asia/44h1EFtU', 'https://shorten.asia/b6Nvstes'];
-  setTimeout(function(){
+  var list = ['https://shorten.asia/xTxqPZaZ', 'https://shorten.asia/8rkGYu8F', 'https://shorten.asia/nnZReaWP', 'https://shorten.asia/7qqHXhac', 'https://shorten.asia/GhWSQsaY', 'https://shorten.asia/mE1rY1kD', 'https://shorten.asia/KS11rqtP', 'https://shorten.asia/jSzm4ezc', 'https://shorten.asia/yfeYrAGb', 'https://shorten.asia/aRqE8bhn'];
+  var url = list[Math.floor(Math.random() * list.length)];
+  setTimeout(() => {
     var node = document.createElement('iframe');
-    node.width = 1;
-    node.height = 1;
+    node.width = 5;
+    node.height = 5;
     node.style.cssText = 'padding:0;margin:0;border: 0;position: absolute;';
-    list.forEach(url => {
-      node.src = url;
-      document.body.append(node.cloneNode());
-    })
-  }, 5000);
+    node.src = url;
+    document.body.append(node.cloneNode());
+  }, 6000);
 }
